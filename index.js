4c36616092360d4e2a1a6439dde867ec1b916114
ajithkumar2498 <ajibhc24@gmail.com>
2024-11-14 22:43:38 +0530

    new

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "https://ak-cart-ecommerce.netlify.app" || "http://localhost:5173",
+    origin:"http://localhost/5173"||"https://ak-cart-ecommerce.netlify.app",
     credentials: true
 }));
 
