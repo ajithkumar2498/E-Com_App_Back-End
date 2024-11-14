40a7add27aa18fea04ecf2480f439336caa57c5a
ajithkumar2498 <ajibhc24@gmail.com>
2024-11-14 21:53:52 +0530

    redux

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "https://ak-cart-ecommerce.netlify.app" ,
+    origin: process.env.FRONTEND_URL || "https://ak-cart-ecommerce.netlify.app" || "http://localhost:5173",
     credentials: true
 }));
 
